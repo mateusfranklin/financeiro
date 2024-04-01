4bb4c8723ce39fbfb0705f96b4b6dc04e429d4ab
Mateus Franklin <contato@mateusfranklin.com>
2024-04-01 10:41:49 -0300

    migarte

--- database/migrations/2024_03_10_133048_create_categories_table.php
+++ database/migrations/2024_03_10_133048_create_categories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,14 +14,38 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('name');
             $table->string('color')->nullable();
             $table->string('icon')->nullable();
+            $table->boolean('section')->default(false);
             $table->integer('sub_category_of')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
+
+
+
+        DB::table('categories')->insert([
+            ['name' => 'Alimentação e Mercado'],
+            ['name' => 'Assinaturas e serviços'],
+            ['name' => 'Bares, Lanchonetes e Restaurantes'],
+            ['name' => 'Compras'],
+            ['name' => 'Educação'],
+            ['name' => 'Família'],
+            ['name' => 'Lazer'],
+            ['name' => 'Transporte'],
+            ['name' => 'Saúde'],
+            ['name' => 'Imposto'],
+            ['name' => 'Pets'],
+            ['name' => 'Trabalho'],
+            ['name' => 'Viagem'],
+            ['name' => 'Outros'],
+        ]);
+
+        DB::table('categories')->insert([
+            ['name' => 'Geral', 'section' => true]
+        ]);
     }
 
     /**
